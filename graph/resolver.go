e3bee324d0db3b391be52f31725205f6adf58a93
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-11-17 20:03:54 -0300

    studying graphql

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/lucassantoss1701/graphql-go/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	Categories []*model.Category
+}
